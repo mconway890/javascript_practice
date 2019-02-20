4c30d47a8df41f7f9c76efde26819e723d40440e
Michele Conway <mconway890@gmail.com>
2019-02-20 14:27:02 -0500

    Add iterator methods to main.js file

--- iterators/main.js
+++ iterators/main.js
@@ -1,4 +1,4 @@
-// forEach method
+// .forEach method
 
 const foods = ['mango', 'papaya', 'pineapple', 'apple', 'egg', 'omelette', 'icepop', 'upsidedown cake'];
 
@@ -10,3 +10,50 @@ foods.forEach(food => {
     console.log(`I want to eat a ${food}.`)
   }
 })
+
+
+// .map() method
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => {
+  return animal[0];
+})
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => {
+  return number/100;
+})
+
+console.log(smallNumbers.join(','));
+
+// .filter() method
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(number => {
+  if (number < 250) {
+    return number;
+  }
+})
+
+console.log(smallNumbers.join(','))
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
+const longFavoriteWords = favoriteWords.filter(word => {
+  if (word.length > 7) {
+    return word;
+  }
+})
+
+console.log(longFavoriteWords.join(', '));

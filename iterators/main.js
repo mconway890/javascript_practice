70b7f23d3cf84c4990c07faaaecab7453dce0efd
Michele Conway <mconway890@gmail.com>
2019-02-20 15:01:22 -0500

    Update readme with iterator lesson, add method practice to main.js

--- iterators/main.js
+++ iterators/main.js
@@ -88,3 +88,49 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 }, 10)
 
 console.log(newSum);
+
+
+// .some() .every() methods
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+const interestingWords = words.filter(word => {
+  return word.length > 5;
+})
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every((word) => {
+	return word.length > 5;
+} ));
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);

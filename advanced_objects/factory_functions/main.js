6d39202d36a2e79cec61428aa1a42ec7fc500203
Michele Conway <mconway890@gmail.com>
2019-03-04 10:09:57 -0500

    ES6 property value shorthand

--- advanced_objects/factory_functions/main.js
+++ advanced_objects/factory_functions/main.js
@@ -13,3 +13,21 @@ const tinCan = robotFactory('P-500', true);
 tinCan.beep(); // Beep Boop
 console.log(tinCan.model); // P-500
 console.log(tinCan.mobile); // true
+
+
+// ES6 property value shorthand
+
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
